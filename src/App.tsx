7f5e894150400132ff0eeeb0ce52ff6db104f3c9
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-23 20:45:02 -0300

    first rendering subs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sub, setSub] = useState([
+    {
+      nick: "dapelu",
+      subMonth: 3,
+      avatar: "https://i.pravatar.cc/150?u=dapelu",
+      descripcion: "dapelu hace de moderador a veces",
+    },
+    {
+      nick: "juan",
+      subMonth: 2,
+      avatar: "https://i.pravatar.cc/150?u=juan",
+    },
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Facu subs</h1>
+      <ul>
+        {sub.map((sub) => {
+          return (
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt="sub image" />
+              <h4>
+                {sub.nick} (<small>{sub.subMonth}</small>)
+              </h4>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
