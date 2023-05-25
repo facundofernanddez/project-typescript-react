a158567e478732ce6b93d457e712c63157cfbfd3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-25 17:07:13 -0300

    create form add subs

---
+++ src/components/Form.tsx
@@ -0,0 +1,16 @@
+export const Form = () => {
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="nick" placeholder="nick" />
+        <input type="text" name="subMonth" placeholder="subMonth" />
+        <input type="text" name="avatar" placeholder="avatar" />
+        <input type="text" name="description" placeholder="description" />
+
+        <button>Save new button!</button>
+      </form>
+    </div>
+  );
+};
